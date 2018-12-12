020447c3b65429f18e5f73a522f39ac10a4aa77b
MEI <1751812903@qq.com>
2018-12-12 10:42:49 +0800

    first version

---
+++ ex5.py
@@ -0,0 +1,23 @@
+my_name = 'Zed A. Shaw'
+
+my_age = 25
+
+my_weight = 180
+
+my_height = 74
+
+my_eyes = 'Brown'
+
+my_teeth = 'White'
+
+my_hair = 'black'
+
+print("Let's talk about %s.")%my_name
+print("He's %d inches tall.")
+print("He's %d pounds heavy")
+print("Actually that's not too heavy")
+print("He's got %s eyes and %s hair")
+print("His teeth  are usually %s depending on the coffee")
+
+#this line is tricky, try to get it exactlty  
+print("If I add %d, %d, and %d I get %d")%(my_age,my_height,my_weight,my_age+my_weight+my_height)
