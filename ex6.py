e3f0c38ecc9813e639b9b5d5166c1ff0e204053d
MEI <1751812903@qq.com>
2018-12-13 20:18:30 +0800

    version2 逐行写说明

--- ex6.py
+++ ex6.py
@@ -1,21 +1,26 @@
 # type in a whole bunch of strings, variables, and formats, and print them
-x = "there are %d types of people"
-binary = "binary"
-do_not = "don't"
-y = "those who know %s and those who %s"#(binary,do_not)
+types_of_people = 10
+#assign 10 to types of people
+x = f"there are {types_of_people} types of people"
+#assign the f-string to the x(insert type_of_people in the string)
 
-print x
-print y
 
-print("I said: %r")#x
-print("I also said:'%s'.")#y
+binary = "binary"#assign "binary" to binary
+do_not = "don't"#assign "don't to do_not"
+y = f"those who know {binary} and those who {do_not}"#(binary,do_not)
+#assign  f-string to the y(insert binary and do_not in the string)
+print(x)
+print(y)
+
+print(f"I said: {x}")#x
+print(f"I also said:'{y}'.")#y
 
 hilarious = false
-joke_evaluation = "Isn't that joke so funny?! %r"
+joke_evaluation = f"Isn't that joke so funny?! {false}"
 
-print joke_evaluation #hilarious
+print (joke_evaluation) #hilarious
 
 w = "This is the left side of ..."
 e = "a string with a right side"
 
-print w+e
+print (w+e)
