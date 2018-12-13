cf695d6f3e28302399336069935ac730fb9178bc
MEI <1751812903@qq.com>
2018-12-13 11:02:22 +0800

    version3 broke

--- ex5.py
+++ ex5.py
@@ -1,3 +1,4 @@
+#1-13创建变量，再到print，将变量作为字符串传进去
 my_name = 'Zed A. Shaw'
 
 my_age = 25
@@ -12,8 +13,8 @@ my_teeth = 'White'
 
 my_hair = 'black'
 
-print(f"Let's talk about {my_name}.")
-print(f"He's {my_height]} inches tall.")
+print("Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
 print(f"He's {my_weight} pounds heavy")
 print(f"Actually that's not too heavy")
 print(f"He's got {my_eyes} eyes and {my_hair} hair")
