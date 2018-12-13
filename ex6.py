9bfad2836ad970d5fc97fbc96226e666bb883fbc
MEI <1751812903@qq.com>
2018-12-13 16:48:31 +0800

    add ex6.py

---
+++ ex6.py
@@ -0,0 +1,21 @@
+# type in a whole bunch of strings, variables, and formats, and print them
+x = "there are %d types of people"
+binary = "binary"
+do_not = "don't"
+y = "those who know %s and those who %s"#(binary,do_not)
+
+print x
+print y
+
+print("I said: %r")#x
+print("I also said:'%s'.")#y
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+print joke_evaluation #hilarious
+
+w = "This is the left side of ..."
+e = "a string with a right side"
+
+print w+e
