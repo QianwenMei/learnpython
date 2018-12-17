eb1ea52e513400a376cffe4b9c84c0dd30b6099a
MEI <1751812903@qq.com>
2018-12-17 15:44:14 +0800

    version3

--- ex8.py
+++ ex8.py
@@ -1,9 +1,10 @@
 formatter = "{} {} {} {}"
 #告诉这个字符串，让它进行格式化
 print(formatter.format(1,2,3,4))
-print(formatter.format("one","two","three","four"))
+print(formatter.format("one","two","three","four"))#true false 的打印可无需加字符串的引号，但是首字母必须大写
 print(formatter.format(True,False,False,True))
-print(formatter.format(formatter,formatter,formatter,formatter))
+#将需要格式化的字符串放到格式化字符串里四次
+print(formatter.format(formatter，formatter，formatter，formatter))
 print(formatter.format(
     "I had this thing.",
     "That you could type up right.",
