e0e3ef1e70a413db4e543a1be263a6d471c0b6fc
MEI <1751812903@qq.com>
2018-12-17 15:57:43 +0800

    version4:repr *lines；

--- ex8.py
+++ ex8.py
@@ -4,10 +4,13 @@ print(formatter.format(1,2,3,4))
 print(formatter.format("one","two","three","four"))#true false 的打印可无需加字符串的引号，但是首字母必须大写
 print(formatter.format(True,False,False,True))
 #将需要格式化的字符串放到格式化字符串里四次
-print(formatter.format(formatter，formatter，formatter，formatter))
-print(formatter.format(
-    "I had this thing.",
+print(formatter.format(formatter, formatter, formatter, formatter))
+
+lines = (
+"I had this thing."
     "That you could type up right.",
     "But it didn't sing.",
     "So I said goodnight."
-))
+)
+print(">>>>>>lines",repr(lines))
+print(formatter.format(*lines))
