df6d8259020d2fa6bf836886246dea8e3f8b1359
MEI <1751812903@qq.com>
2018-12-18 16:24:16 +0800

    version1:显示年月

---
+++ ex9.py
@@ -0,0 +1,15 @@
+#here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue wen Thu Fri Sat Sun"
+months = "Jan/Feb/Mar/Apr/May/Jun/July/Augu"
+
+print("Here are the days:",days)
+
+print("Here are the months",months)
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines of we want,or 5,or 6.
+""")
