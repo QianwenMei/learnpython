44241764d4f7169452ceac66a497c5dc82fb82a3
MEI <1751812903@qq.com>
2018-12-19 16:36:28 +0800

    version1

---
+++ ex11.py
@@ -0,0 +1,10 @@
+print("How old are u?")
+age = raw_input()
+
+print("How tall are u?")
+height = raw_input
+
+print("How much do u weigh?")
+weight = raw_input()
+
+print("So,you're %r old, %r tall and %r heavy.")%(age,height,weight)
