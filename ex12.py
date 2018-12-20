2df41c65ab95f984aafdffb243f4d90c65d6409d
MEI <1751812903@qq.com>
2018-12-20 16:41:56 +0800

    version1

---
+++ ex12.py
@@ -0,0 +1,13 @@
+#new
+age = input("How old are you?")
+
+height = input("How tall are you?")
+
+weight = input("How much do you weigh?")
+
+print(f"So,you're {age} old,{height} tall and {weight} heavy.")
+
+#before
+print("How old are you?",end = ' ')
+age = input()
+print(f"So you are {age} old.")
