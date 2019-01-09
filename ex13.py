bd96efd016e3f1799bc4b9246b24c592f687aa10
MEI <1751812903@qq.com>
2019-01-09 10:54:56 +0800

    version3:what is argv

--- ex13.py
+++ ex13.py
@@ -1,7 +1,12 @@
 from sys import argv#使用后即可获取命令行参数
 
-script，first,second,third = argv
+script,first,second,third = argv
 #从argv中得到四个参数，只需三个，但历史惯例，脚本会有一个额外参数，scriopt是脚本名称
+
+
+print(">>>>>>>>>>>argv=",repr(argv))
+
+
 
 print("The script is called:",script)
 print("Your first variable is:",first)
@@ -14,3 +19,6 @@ print("Your third variable is:",third)
 #Your first variable is: apple
 #Your second variable is: orange
 #Your third variable is: dragonfruit
+
+#>>>>>>>>>>>argv= ['ex13.py', 'apple', 'orange', 'dargonfruit']
+#看起来像参数，目前只知道它是一个列表，python把命令行参数取来，放到列表中去，再把它们分别拆开，放到每个变量中去
