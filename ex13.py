914f9a60946dd50a59cd6e693993e3a2b56e3e10
MEI <1751812903@qq.com>
2019-01-09 10:45:20 +0800

    version2:how to run

--- ex13.py
+++ ex13.py
@@ -1,9 +1,16 @@
 from sys import argv#使用后即可获取命令行参数
 
-script,first,second,third = argv
+script，first,second,third = argv
 #从argv中得到四个参数，只需三个，但历史惯例，脚本会有一个额外参数，scriopt是脚本名称
 
 print("The script is called:",script)
 print("Your first variable is:",first)
 print("Your second variable is:",second)
 print("Your third variable is:",third)
+
+
+#powershell中输入 python ex13.py apple orange dragonfruit
+#The script is called: ex13.py-----------打印出了脚本名称
+#Your first variable is: apple
+#Your second variable is: orange
+#Your third variable is: dragonfruit
